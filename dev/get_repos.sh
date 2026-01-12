621e1f369783188afb7ec62c5e58b13da86f3a91
lukas-png <doenges@uni-hildesheim.de>
2026-01-12 15:39:42 +0100

    added support for defectsj v1

---
+++ dev/get_repos.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+set -e
+
+# The name of the archive that contains all project repos
+# For Defects4J v1.x use the legacy bundle; v3 bundle is truncated and
+# misses commits (e.g., Lang-1) when paired with v1.2.0.
+ARCHIVE=defects4j-repos.zip
+
+main() {
+    # The BSD version of stat does not support --version or -c
+    if stat --version &> /dev/null; then
+        # GNU version
+        cmd="stat -c %Y $ARCHIVE"
+    else
+        # BSD version
+        cmd="stat -f %m $ARCHIVE"
+    fi
+
+    if [ -e $ARCHIVE ]; then
+        old=$($cmd)
+    else
+        old=0
+    fi
+    # Only download repos if the server has a newer file
+    download_url "https://defects4j.org/downloads/$ARCHIVE"
+
+    new=$($cmd)
+
+    # Exit if no newer file is available
+    [ "$old" == "$new" ] && exit 0
+
+    # Remove old files
+    clean
+
+    # Extract new repos
+    unzip -q -u $ARCHIVE && mv defects4j/project_repos/* . && rm -r defects4j
+}
+
+clean() {
+    rm -rf \
+    closure-compiler.git \
+    commons-cli.git \
+    commons-codec.git \
+    commons-collections.git \
+    commons-compress.git \
+    commons-csv.git \
+    commons-jxpath.git \
+    commons-lang.git \
+    commons-math.git \
+    gson.git \
+    jackson-core.git \
+    jackson-databind.git \
+    jackson-dataformat-xml.git \
+    jfreechart \
+    jfreechart.git \
+    joda-time.git \
+    jsoup.git \
+    mockito.git \
+    repos.csv \
+    sync.sh \
+    README 
+}
+
+# MacOS does not install the timeout command by default.
+if [ "$(uname)" = "Darwin" ] ; then
+  function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+fi
+
+# Download the remote resource to a local file of the same name, if the
+# remote resource is newer.  Works around connections that hang.  Takes a
+# single command-line argument, a URL.
+download_url() {
+    BASENAME=`basename ${@: -1}`
+    if [ "$(uname)" = "Darwin" ] ; then
+        wget -nv -N "$@"
+    else
+	timeout 300 curl -s -S -R -L -O -z "$BASENAME" "$@" || (echo "retrying curl $@" && rm -f "$BASENAME" && curl -R -L -O -z "$BASENAME" "$@")
+    fi
+}
+
+main
